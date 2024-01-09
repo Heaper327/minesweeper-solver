9daf2cad29391b11a6f293973b366d0839fba0f0
Heaper327 <jameszhu327@outlook.com>
2024-01-09 12:10:17 +0800

    Add flagging and rulers to interactive interface

--- interactive.py
+++ interactive.py
@@ -9,18 +9,39 @@ def main():
 
     while not (game.hasWon() or game.hasLost()):
         inputStr = input("To open a tile, input 'o row col', to flag a tile, input 'f row col': ")
+        
         tokens = inputStr.split(' ')
         if len(tokens) != 3:
             print("Malformed input, please try again")
             continue
-        row, col = int(tokens[0]), int(tokens[1])
-        game.open(row, col)
+        action = tokens[0]
+        row = int(tokens[1]) - 1 # Convert from 1-indexed to 0-indexed
+        col = int(tokens[2]) - 1
+
+        if action == 'f':
+            game.flag(row, col)
+        elif action == 'o':
+            game.open(row, col)
+        else:
+            print("Action cannot be recognized, make sure it is either r or o")
+
         printBoard(game)
 
-def printBoard(game):
-    board = game.toCharsPretty()
-    print("\n".join(["".join(row) for row in board]))
-    print("")
+    if game.hasWon():
+        print("You won!")
+    else:
+        print("Game over, you lost!")
+
+def printBoard(game: Game):
+    board = game.getBoardPretty()
+    # Tick every 5 tiles
+    xRuler = "         " + "         ".join(['X' for i in range(int(game.numCols / 5))])
+    yRuler = ['X' if i % 5 == 4 else ' ' for i in range(game.numRows)]
+
+    rows = [yRuler[row] + "".join(board[row]) for row in range(game.numRows)]
+    rows.insert(0, xRuler)
+
+    print("\n".join(rows))
     
 
 if __name__ == '__main__':
